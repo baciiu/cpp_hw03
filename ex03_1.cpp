03386ff37e9921d8fef257a6f40abce85def8093
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2022-11-10 14:46:03 +0100

    Exercise 1 - BubbleSort with Template

--- ex03_1.cpp
+++ ex03_1.cpp
@@ -4,6 +4,38 @@
 
 using namespace std;
 
+template <typename T>
+void bubbleSort(T array[], int n){
+
+    for( int i = 0 ; i < n-1; ++i ){
+        for( int j = 0 ; j < n-i-1 ; ++j ){
+
+            if( array[j] > array[j+1] ){
+                T temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+template<typename T>
+void printArray(T array[], int n){
+    for (int i = 0; i < n; ++i) {
+        cout << array[i] << " ";
+    }
+    cout<< "\n\n";
+}
+
+
+
+
 void Exercise31::run(){
  cout << "Exercise 1" << endl;
+ int array[] = { 1, 10, 90, 100, -1, 11, 9, 14, 3, 2, 20, 19 };
+ int n = sizeof(array) / sizeof(int);
+
+    printArray(array,n);
+    bubbleSort(array,n);
+    printArray(array,n);
+
 }
