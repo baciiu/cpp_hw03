592ae11b732aa23c4c714e545452f8fab157528d
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2022-11-11 12:05:13 +0100

    Exercise 2 - template class Number

--- ex03_2.cpp
+++ ex03_2.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+template <class T>
+class Number {
+private:
+    T number;
+
+public:
+    Number(T n) : number(n) {}   // constructor
+
+    T getNumber() {
+        return number;
+    }
+};
+
+
 void Exercise32::run(){
 cout << "Exercise 2" << endl;
+
+    Number<int> numberInt(7);
+    Number<double> numberDouble(7.7);
+
+    cout << "int Number = " << numberInt.getNumber() << endl;
+    cout << "double Number = " << numberDouble.getNumber() << endl;
+
 }
